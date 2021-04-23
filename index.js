2744e8615b26499a6864a4c15375587365fca0b6
Petr Prudil <petrprudil2005@seznam.cz>
2021-04-23 16:25:38 +0200

    setup1

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 exports.log = function (content = "I'm working!") {
     console.log('WedGE: ' + content)
 };
+
+exports.setup = function (htmlclass = ".gameCanvas") {
+    console.log('WedGE: Starting Setup')
+    const canvas = document.querySelector('canvas')
+    console.log('WedGE: Found a canvas - ' + canvas)
+    c = canvas.getContext
+};
